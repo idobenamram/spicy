2df7def987f0091c2ff3d34e947a9272a7988070
Ido Ben Amram <idoba12@gmail.com>
2025-10-14 10:30:35 +0300

    fix fuzz target

--- fuzz/fuzz_targets/spicy_parser.rs
+++ fuzz/fuzz_targets/spicy_parser.rs
@@ -1,10 +1,18 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use spicy_parser::instance_parser::parse;
+use spicy_parser::{parse, ParseOptions, SourceMap};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = parse(s);
+        let path = std::path::PathBuf::from("fuzz_input.spicy");
+        let source_map = SourceMap::new(path.clone(), s.to_string());
+        let mut options = ParseOptions {
+            work_dir: std::path::PathBuf::from("."),
+            source_path: path,
+            source_map,
+            max_include_depth: 0,
+        };
+        let _ = parse(&mut options);
     }
 });
